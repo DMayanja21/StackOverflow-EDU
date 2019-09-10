70715f59e918c87590cc12021063df020fc09bdb
DMayanja21 <you@example.com>
2019-09-10 18:14:05 +0300

    updates with bootstrap

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express=require('express');
 const app=express();
-const path=require('path');
-const port=process.env.PORT || 3000
+//calls the express application to run 
 
-app.use(express.static('public'));
+const path=require('path');
+const port=process.env.PORT || 3000 
+//defines which port the index .html will open from
+
+app.use(express.static('Itron'));
+//defines the folder from which additional pages and files needed by index,html will be obtained
 
 app.get('/*',(req,res)=>{
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
+//specifies the openning file
 
 app.listen(port, ()=>{
 	console.log('StackOverflow-EDU is running on port'+ port);
 });
+//names the working port i n the console
